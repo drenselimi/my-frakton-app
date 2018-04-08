15672de5aa778b62afa8aa3994216962b063de73
Dren Selimi <selimi.dreni@gmail.com>
2018-04-08 19:36:30 +0200

    v1.2

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -8,22 +8,11 @@ import 'rxjs/Rx';
 @Injectable()
 export class UserService {
 
-  // url: string = 'http://localhost:3000/employees';
-
   url: string = 'https://jsonplaceholder.typicode.com/users?_sort=name&_order=asc';
 
   constructor(
     private http: HttpClient
   ) { }
-
-  // getAll() {
-  //   return this.http.get<User[]>(this.url)
-  //     .map((data: User[]) => {
-  //       return data;
-  //     })
-
-
-  // }
 
   getAll() {
     return this.http.get<User[]>(this.url)
